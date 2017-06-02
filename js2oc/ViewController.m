6ee2e087da91cc01e9f5578948ce462fb4ce1078
huangbo <huangbo@yy.com>
2017-06-02 10:42:55 +0800

    oc与js互相调用的方式

---
+++ js2oc/ViewController.m
@@ -0,0 +1,104 @@
+//
+//  ViewController.m
+//  js2oc
+//
+//  Created by boob on 2017/6/2.
+//  Copyright © 2017年 YY.COM. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <JavaScriptCore/JavaScriptCore.h>
+@interface ViewController ()<UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+@property (weak, nonatomic) IBOutlet UIButton *btnoc2js;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.webview.delegate = self;
+    [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"test2" ofType:@"html"]]]];
+    
+     }
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    JSContext *context = [self.webview valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    //定义好JS要调用的方法, share就是调用的share方法名
+    context[@"share"] = ^() {
+        NSLog(@"+++++++Begin Log+++++++");
+        NSArray *args = [JSContext currentArguments];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"方式二" message:@"这是OC原生的弹出窗" delegate:self cancelButtonTitle:@"收到" otherButtonTitles:nil];
+            [alertView show];
+        });
+        
+        for (JSValue *jsVal in args) {
+            NSLog(@"%@", jsVal.toString);
+        }
+        
+        NSLog(@"-------End Log-------");
+    };
+}
+
+#pragma mark - UIWebViewDelegate
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    NSURL * url = [request URL];
+    if ([[url scheme] isEqualToString:@"firstclick"]) {
+        NSArray *params =[url.query componentsSeparatedByString:@"&"];
+        
+        NSMutableDictionary *tempDic = [NSMutableDictionary dictionary];
+        for (NSString *paramStr in params) {
+            NSArray *dicArray = [paramStr componentsSeparatedByString:@"="];
+            if (dicArray.count > 1) {
+                NSString *decodeValue = [dicArray[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+                [tempDic setObject:decodeValue forKey:dicArray[0]];
+            }
+        }
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"方式一" message:@"这是OC原生的弹出窗" delegate:self cancelButtonTitle:@"收到" otherButtonTitles:nil];
+        [alertView show];
+        NSLog(@"tempDic:%@",tempDic);
+        return NO;
+    }
+    
+    return YES;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+/**
+ * 第一种方式调用js
+ */
+- (IBAction)oc2jsbtntap:(id)sender {
+
+    NSString *jsStr = [NSString stringWithFormat:@"showAlert('%@')",@"这里是JS中alert弹出的message"];
+    [_webview stringByEvaluatingJavaScriptFromString:jsStr];
+    
+    /**
+     * stringByEvaluatingJavaScriptFromString是一个同步的方法，使用它执行JS方法时，如果JS 方法比较耗的时候，会造成界面卡顿。尤其是js 弹出alert 的时候。
+     alert 也会阻塞界面，等待用户响应，而stringByEvaluatingJavaScriptFromString又会等待js执行完毕返回。这就造成了死锁。
+     
+     官方推荐使用WKWebView的evaluateJavaScript:completionHandler:代替这个方法。
+     其实我们也有另外一种方式，自定义一个延迟执行alert 的方法来防止阻塞，然后我们调用自定义的alert 方法。同理，耗时较长的js 方法也可以放到setTimeout 中。 
+     */
+}
+
+/**
+ * 第二种方式
+ */
+- (IBAction)oc2jsbtntap2:(id)sender {
+
+    JSContext *context = [self.webview valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    NSString *textJS = @"showAlert('这里是JS中alert弹出的message')";
+    [context evaluateScript:textJS];
+}
+
+@end
